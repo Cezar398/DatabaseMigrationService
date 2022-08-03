d2d72517aae5d5d44c7451a0e287b42eb7f07c40
Cezar398 <69121609+Cezar398@users.noreply.github.com>
2022-08-03 16:12:27 +0300

    Added api-docs

--- src/main/java/com/assist/internship/migrationService/api/v1/migration/MigrationController.java
+++ src/main/java/com/assist/internship/migrationService/api/v1/migration/MigrationController.java
@@ -1,14 +1,17 @@
 package com.assist.internship.migrationService.api.v1.migration;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MigrationController {
 
+    MigrationService migrationService;
+
     @PostMapping(path = "/start")
-    public void start()
+    public void start(@RequestBody String url)
     {
-        System.out.println("start");
+         migrationService.start(url);
     }
 }
--- src/main/java/com/assist/internship/migrationService/api/v1/migration/MigrationService.java
+++ src/main/java/com/assist/internship/migrationService/api/v1/migration/MigrationService.java
@@ -1,4 +1,11 @@
 package com.assist.internship.migrationService.api.v1.migration;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class MigrationService {
+    public void start(String url)
+    {
+
+    }
 }
