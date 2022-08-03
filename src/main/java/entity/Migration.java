0d2c98eeb4c6056bc2866dd1bdfea07c20102dd7
Cezar398 <69121609+Cezar398@users.noreply.github.com>
2022-08-03 11:35:04 +0300

    Entity data updated

--- src/main/java/entity/Migration.java
+++ src/main/java/entity/Migration.java
@@ -2,7 +2,18 @@ package entity;
 
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class Migration {
     private String id;
+    private String title;
+    private String overview;
+    private String poster_path;
+    private String media_type;
+    private String popularity;
+    private Date release_date;
+    private boolean video;
+    private float vote_average;
+    private int vote_count;
 }
