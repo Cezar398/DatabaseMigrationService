bf993f6401b0643cfcee3287eb3fd917ffe5fa5b
Cezar398 <69121609+Cezar398@users.noreply.github.com>
2022-08-03 16:04:23 +0300

    Add start endpoint

--- src/main/java/com/assist/internship/migrationService/api/v1/migration/MigrationController.java
+++ src/main/java/com/assist/internship/migrationService/api/v1/migration/MigrationController.java
@@ -1,4 +1,14 @@
 package com.assist.internship.migrationService.api.v1.migration;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MigrationController {
+
+    @PostMapping(path = "/start")
+    public void start()
+    {
+        System.out.println("start");
+    }
 }
