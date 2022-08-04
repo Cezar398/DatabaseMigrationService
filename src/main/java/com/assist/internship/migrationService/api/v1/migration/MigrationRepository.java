ab78429446b47539fd3523b85e6daf4f65267ddb
Cezar398 <69121609+Cezar398@users.noreply.github.com>
2022-08-04 17:36:04 +0300

    Added CRUD for movie entity

--- src/main/java/com/assist/internship/migrationService/api/v1/migration/MigrationRepository.java
+++ src/main/java/com/assist/internship/migrationService/api/v1/migration/MigrationRepository.java
@@ -4,6 +4,9 @@ import com.assist.internship.migrationService.entity.Migration;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MigrationRepository extends JpaRepository<Migration, String> {
+
 }
--- src/main/java/com/assist/internship/migrationService/entity/Migration.java
+++ src/main/java/com/assist/internship/migrationService/entity/Migration.java
@@ -1,16 +1,17 @@
 package com.assist.internship.migrationService.entity;
 
 import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Data
 @Entity
 @Table(name = "movie", schema = "public")
 public class Migration {
     @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
     private String title;
     private String overview;
@@ -21,4 +22,6 @@ public class Migration {
     private Boolean video;
     private Float vote_average;
     private Integer vote_count;
+
+
 }
