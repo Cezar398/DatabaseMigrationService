eb9cabe039ee9da7dd867bda95606f9a9786c5da
Cezar398 <69121609+Cezar398@users.noreply.github.com>
2022-08-10 11:38:02 +0300

    - Deleted MigrationDto.java

--- src/main/java/com/assist/internship/migrationservice/api/v1/movie/dto/MigrationDto.java
+++
@@ -1,9 +0,0 @@
-package com.assist.internship.migrationservice.api.v1.movie.dto;
-
-import lombok.Data;
-
-@Data
-public class MigrationDto {
-   private String username;
-   private String password;
-}
